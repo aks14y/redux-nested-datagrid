7dc8dbc0e5cf60062d19e1276fdd3750e63b69ad
AXA77 <AXA77@acuitysso.com>
2023-09-20 15:11:46 +0530

    feat: nested table with mockData

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,9 @@
+import App from "./App";
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+    <section data-testid="main-container">
+      <App />
+    </section>
+  );
 }
